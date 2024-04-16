fc40757bd4b4c319347f8f57ed05a6e6acb6d501
Mbr90 <49838649+Mbr90@users.noreply.github.com>
2024-04-16 21:58:05 +0200

    changed api

--- src/components/fetchData.jsx
+++ src/components/fetchData.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const url = "http://localhost:1337/api/venues?populate=Forsidebilde";
+const url = "http://localhost:1337/api/venues?populate=MainImage";
 
 export default function FetchData() {
   const [data, setData] = useState([]);
@@ -27,49 +27,27 @@ export default function FetchData() {
     }
 
     getData();
-   
   }, []);
 
   if (isLoading) {
-    return <div>Loading results..</div>
+    return <div>Loading results..</div>;
   }
 
   if (isError) {
-    return <div>Error loading data</div>
+    return <div>Error loading data</div>;
   }
 
-  
-//   let tittel = posts.data[0].attributes.Tittel
-
-
-  
-  console.log(data)
-
-  let imageUrls = []
-  
-  data.forEach(post => {
-    imageUrls.push("http://localhost:1337"+post.attributes.Forsidebilde.data.attributes.url)
-  });
-
-  console.log(imageUrls)
-    
-//   let bildeUrl = posts.data[0].attributes.Forsidebilde.data.attributes.url
-
-//   console.log(bildeUrl)
-
- 
-
-  return(
-    <div>Check console log for results. <img
-    className="h-96 w-96 object-contain mx-auto"
-    src={imageUrls[0]}
-    
-  /> <h1></h1> </div>
-  )
-
-
-
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>
+          {/* <img src={item.attributes.MainImage.data.attributes.url} /> */}
+          {/* <p>{item.attributes.MainImage.attributes.provider}</p> */}
+          <h1 className="text-xl">{item.attributes.Title}</h1>
+          <p>{item.attributes.Description}</p>
+        </div>
+      ))}
+      <h1></h1>{" "}
+    </div>
+  );
 }
-
-
-{/* <img src={data.data[0].attributes.Forsidebilde.data.attributes.formats.thumbnail.url} alt={data.data[0].attributes.Tittel}></img> */}
