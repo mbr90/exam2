16874e252311e6e79be06026bbde1ed02574ed8c
Mbr90 <49838649+Mbr90@users.noreply.github.com>
2024-05-05 21:42:55 +0200

    SearchBar styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -107,111 +107,103 @@ export default function SearchBar({ venues }) {
   }, [checkInDate, checkOutDate, setCheckOutDate]);
 
   return (
-    <div className="flex flex-col w-full">
-      <ul className="flex gap-2 mx-auto border-solid border-2 border-black p-4 rounded-full">
-        <li className="flex clear-start hover:cursor-pointer relative">
-          <MdOutlineSearch />
-          <input
-            ref={inputRef}
-            type="text"
-            placeholder="Where to"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-            onFocus={() => setIsInputFocused(true)}
-            onBlur={() => setTimeout(() => setIsInputFocused(false), 300)}
-            className="ml-2 outline-none cursor-pointer"
-          />
+    <ul className="flex gap-2 mx-auto  p-4 rounded-full text-charcoal">
+      <li className="flex my-auto clear-start hover:cursor-pointer relative">
+        <MdOutlineSearch />
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Where to"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          onFocus={() => setIsInputFocused(true)}
+          onBlur={() => setTimeout(() => setIsInputFocused(false), 300)}
+          className="ml-2 outline-none cursor-pointer placeholder-charcoal"
+        />
 
-          {isInputFocused && filterVenues.length > 0 && (
-            <ul className="absolute top-11 left-1/2 transform -translate-x-1/2 text-black flex flex-col w-64 bg-white z-50 p-2">
-              {filterVenues.map((loc) => (
-                <li
-                  key={loc}
-                  onClick={() => handleLocationSelect(loc)}
-                  className="p-2 hover:bg-gray-200 cursor-pointer"
-                >
-                  {loc}
-                </li>
-              ))}
-            </ul>
-          )}
-        </li>
-        <li className="flex hover:cursor-pointer">
-          <MdOutlineDateRange />
-          <ReactDatePicker
-            selected={checkInDate}
-            onChange={(date) => setCheckInDate(date)}
-            minDate={new Date()}
-            dateFormat="dd/MM/yyyy"
-            placeholderText="Check-in"
-            className="cursor-pointer"
-          />
-        </li>
-        <li className="flex hover:cursor-pointer">
-          <MdOutlineDateRange />
-          <ReactDatePicker
-            selected={checkOutDate}
-            onChange={(date) => setCheckOutDate(date)}
-            minDate={
-              checkInDate ? add(new Date(checkInDate), { days: 1 }) : new Date()
-            }
-            dateFormat="dd/MM/yyyy"
-            placeholderText="Check-out"
-            className="ml-2 outline-none cursor-pointer"
-          />
-        </li>
-        <li className="flex items-center relative">
-          <MdOutlinePermIdentity />
-          <button
-            ref={buttonRef}
-            onClick={toggleGuestsDropdown}
-            className="ml-2"
+        {isInputFocused && filterVenues.length > 0 && (
+          <ul className="absolute top-11 left-1/2 transform -translate-x-1/2 text-charcoal flex flex-col w-64 bg-white z-50 p-2">
+            {filterVenues.map((loc) => (
+              <li
+                key={loc}
+                onClick={() => handleLocationSelect(loc)}
+                className="p-2 hover:bg-gray-200 cursor-pointer"
+              >
+                {loc}
+              </li>
+            ))}
+          </ul>
+        )}
+      </li>
+      <li className="flex my-auto hover:cursor-pointer">
+        <MdOutlineDateRange />
+        <ReactDatePicker
+          selected={checkInDate}
+          onChange={(date) => setCheckInDate(date)}
+          minDate={new Date()}
+          dateFormat="dd/MM/yyyy"
+          placeholderText="Check-in"
+          className="cursor-pointer placeholder-charcoal"
+        />
+      </li>
+      <li className=" flex my-auto hover:cursor-pointer">
+        <MdOutlineDateRange />
+        <ReactDatePicker
+          selected={checkOutDate}
+          onChange={(date) => setCheckOutDate(date)}
+          minDate={
+            checkInDate ? add(new Date(checkInDate), { days: 1 }) : new Date()
+          }
+          dateFormat="dd/MM/yyyy"
+          placeholderText="Check-out"
+          className=" flexml-2 outline-none cursor-pointer placeholder-charcoal"
+        />
+      </li>
+      <li className="flex items-center relative">
+        <MdOutlinePermIdentity />
+        <button ref={buttonRef} onClick={toggleGuestsDropdown} className="ml-2">
+          Who?
+        </button>
+        {isGuestsOpen && (
+          <div
+            ref={dropdownRef}
+            className="absolute top-full  bg-cloud border border-ash rounded-lg p-2 shadow-lg"
           >
-            Who?
-          </button>
-          {isGuestsOpen && (
-            <div
-              ref={dropdownRef}
-              className="absolute top-full  bg-cloud border border-ash rounded-lg p-2 shadow-lg"
-            >
-              {Object.keys(guests).map((type) => (
-                <div
-                  key={type}
-                  className="flex justify-between items-center my-1"
-                >
-                  <span>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
-                  <div className="flex items-center">
-                    <span className="text-deepsea">
-                      <button
-                        onClick={() =>
-                          handleParticipantChange(type, "decrement")
-                        }
-                        className="p-1"
-                      >
-                        <MdRemoveCircleOutline size={24} />
-                      </button>
-                    </span>
-
-                    <span className="mx-2">{guests[type]}</span>
+            {Object.keys(guests).map((type) => (
+              <div
+                key={type}
+                className="flex justify-between items-center my-1"
+              >
+                <span>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
+                <div className="flex items-center">
+                  <span className="text-deepsea">
                     <button
-                      onClick={() => handleParticipantChange(type, "increment")}
+                      onClick={() => handleParticipantChange(type, "decrement")}
                       className="p-1"
                     >
-                      <span className="text-deepsea">
-                        <MdAddCircleOutline size={24} />
-                      </span>
+                      <MdRemoveCircleOutline size={24} />
                     </button>
-                  </div>
+                  </span>
+
+                  <span className="mx-2">{guests[type]}</span>
+                  <button
+                    onClick={() => handleParticipantChange(type, "increment")}
+                    className="p-1"
+                  >
+                    <span className="text-deepsea">
+                      <MdAddCircleOutline size={24} />
+                    </span>
+                  </button>
                 </div>
-              ))}
-            </div>
-          )}
-        </li>
-        <li>
-          <BackgroundButton text={"Clear"} onClick={() => clearSearchStore()} />
-        </li>
-      </ul>
-    </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </li>
+      <li>
+        <BackgroundButton text={"Clear"} onClick={() => clearSearchStore()} />
+      </li>
+    </ul>
   );
 }
 
